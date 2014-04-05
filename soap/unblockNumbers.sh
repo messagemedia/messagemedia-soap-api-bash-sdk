f2a08476ea8707390fd9c76ac8a16b844b1482c2
Paul Colby <git@colby.id.au>
2014-04-05 13:36:19 +1100

    Added initial soap/{un,}blockNumbers.sh scripts.

---
+++ soap/unblockNumbers.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#
+# Example shell script for unblocking numbers via MessageMedia's SOAP API.
+#
+# Run with --help for basic usage.  See the accompanying README.md file for
+# detailed usage information.
+#
+
+. "$(dirname $(readlink -f $0))/auth.inc.sh"
+. "$(dirname $(readlink -f $0))/functions.inc.sh"
+
+# Some defaults.
+ENDPOINT='https://soap.m4u.com.au'
+RECIPIENT_ID=0
+
+# Paths to required commands.
+CURL=`which curl`
+SED=`which sed`
+
+# Show a basic, standardise usage message.
+showUsage() {
+    echo -e "\nUsage: "`basename $0`" [options] phone1 [phone2 [...]]\n" >&2
+    exit 128
+}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --debug)
+            DEBUG=true
+            ;;
+        --dryrun|--dry-run)
+            DRYRUN=true
+            ;;
+        --help)
+            showUsage
+            ;;
+        --recipient-id)
+            requireArg $1 $#
+            RECIPIENT_ID=`echo "$2" | "$SED" -e 's/&/\&amp;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g' -e 's/"/\&quot;/g'`
+            shift
+            ;;
+        *)
+            RECIPIENT=`echo "$1" | "$SED" -e 's/&/\&amp;/g' -e 's/</\&lt;/g' -e 's/>/\&gt;/g' -e 's/"/\&quot;/g'`
+            RECIPIENTS[$RECIPIENT_ID]="$RECIPIENT"
+            RECIPIENT_ID=$(( $RECIPIENT_ID + 1 ))
+            ;;
+    esac
+    shift
+done
+
+# Ensure we have at least one recipient to block.
+if [ ${#RECIPIENTS[@]} -eq 0 ]; then
+    showUsage
+fi
+
+CHARSET=`echo ${LANG} | ${SED} 's/^.*\.//' | tr 'A-Z' 'a-z-'`
+
+SOAP_REQUEST="<?xml version=\"1.0\" encoding=\"$CHARSET\"?>
+<env:Envelope xmlns:env=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"http://xml.m4u.com.au/2009\">
+ <env:Body>
+  <ns:unblockNumbers>
+   <ns:authentication>
+    <ns:userId>$MESSAGEMEDIA_USERID</ns:userId>
+    <ns:password>$MESSAGEMEDIA_PASSWORD</ns:password>
+   </ns:authentication>
+   <ns:requestBody>
+    <ns:recipients>"
+
+for RECIPIENT_ID in "${!RECIPIENTS[@]}"; do
+    SOAP_REQUEST="$SOAP_REQUEST
+     <ns:recipient uid=\"$RECIPIENT_ID\">${RECIPIENTS[$RECIPIENT_ID]}</ns:recipient>"
+done
+
+SOAP_REQUEST="$SOAP_REQUEST
+    </ns:recipients>
+   </ns:requestBody>
+  </ns:unblockNumbers>
+ </env:Body>
+</env:Envelope>"
+
+# If debugging, log the request.
+if [ -n "$DEBUG" ]; then
+    echo; echo "$SOAP_REQUEST"; echo
+fi
+
+# If not performing a dryrun, carry out the SOAP request.
+if [ -z "$DRYRUN" ]; then
+    "$CURL" --data-binary "$SOAP_REQUEST" --header "Content-type: text/xml; charset=$CHARSET" "$ENDPOINT"
+fi
