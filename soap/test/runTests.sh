0209ab8dae648c967f56a3b5d0dd1b8717f6352c
Paul Colby <git@colby.id.au>
2014-04-05 17:06:00 +1100

    A splash of color.

--- soap/test/runTests.sh
+++ soap/test/runTests.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DIFF=`which colordiff`
+if [ -z "$DIFF" ]; then DIFF=`which diff`; fi
+
 if [ $# -gt 0 ]; then
     TESTS=( $@ )
 else
@@ -11,12 +14,12 @@ for TEST in "${TESTS[@]}"; do
     TEST=`basename "$TEST" .sh`
     echo -n "Test: $TEST"
     "./$TEST.sh" > "/tmp/soap.$TEST.xml"
-    OUTPUT=`diff "$TEST.xml" "/tmp/soap.$TEST.xml"`
+    OUTPUT=`"$DIFF" "$TEST.xml" "/tmp/soap.$TEST.xml"`
     if [ $? -eq 0 ]; then
-        echo ' passed'
+        echo -e ' \x1b[32mpassed\x1b[0m'
         PASSED=$(( $PASSED + 1))
     else
-        echo ' failed'
+        echo -e ' \x1b[31mfailed:\x1b[0m'
         echo "$OUTPUT"
     fi
 done
